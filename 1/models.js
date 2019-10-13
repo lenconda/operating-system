fc4acbe00320e9308ff02d4c64e3833b4bb69576
lenconda <i@lenconda.top>
2019-10-13 16:23:31 +0800

    :art: Improve code

---
+++ 1/models.js
@@ -0,0 +1,41 @@
+class GenericModel {
+  constructor(id, destination, queue, rope, delay) {
+    this.id = id;
+    this.destination = destination;
+    this.waiting = true;
+    this.crossing = false;
+    this.crossed = false;
+    this.queue = queue;
+    this.rope = rope;
+    this.delay = delay;
+    this.queue.push(this);
+  }
+
+  waitUntilSafeToCross() {
+    this.waiting = true;
+    this.crossing = false;
+    this.crossed = false;
+  }
+
+  crossRavine() {
+    this.waiting = false;
+    this.crossing = true;
+    this.crossed = false;
+    this.queue.shift();
+    this.rope.push(this);
+    setTimeout(() => this.doneWithCrossing(), this.delay);
+  }
+
+  doneWithCrossing() {
+    this.waiting = false;
+    this.crossing = false;
+    this.crossed = true;
+    this.rope.shift();
+  }
+}
+
+class Pig extends GenericModel {};
+
+class Student extends GenericModel {};
+
+exports = module.exports = { Pig, Student };
--- 1/pig.js
+++ 1/pig.js
@@ -1,37 +1,59 @@
-class Pig {
-  constructor(id, destination, queue, rope, delay) {
-    this.id = id;
-    this.destination = destination;
-    this.waiting = true;
-    this.crossing = false;
-    this.crossed = false;
-    this.queue = queue;
-    this.rope = rope;
-    this.delay = delay;
-    this.queue.push(this);
+const { Pig } = require('./models');
+
+const toWestQueue = [], toEastQueue = [], rope = [];
+
+function generator(id) {
+  const destination = Math.round(Math.random()) === 0 ? 'west' : 'east';
+  new Pig(id, destination, (destination === 'west' ? toWestQueue : toEastQueue), rope, 5000);
+}
+
+for (let i = 0; i < 50; i++) {
+  generator(i);
+}
+
+async function delay(ms) {
+  return new Promise(resolve => {
+    setTimeout(() => { resolve(); }, ms);
+  });
+}
+
+function checkAvail(destination) {
+  for (let i = 0; i < rope.length; i++) {
+    if (rope[i].destination !== destination) {
+      return false;
+    }
   }
 
-  waitUntilSafeToCross() {
-    this.waiting = true;
-    this.crossing = false;
-    this.crossed = false;
-  }
+  return true;
+}
 
-  crossRavine() {
-    this.waiting = false;
-    this.crossing = true;
-    this.crossed = false;
-    this.queue.shift();
-    this.rope.push(this);
-    setTimeout(() => this.doneWithCrossing(), this.delay);
-  }
+async function start() {
+  const destination = Math.round(Math.random()) === 0 ? 'west' : 'east';
 
-  doneWithCrossing() {
-    this.waiting = false;
-    this.crossing = false;
-    this.crossed = true;
-    this.rope.shift();
+  destination === 'west' ? toWestQueue[0].crossRavine() : toEastQueue[0].crossRavine();
+
+  while (toWestQueue.length > 0 || toEastQueue.length > 0) {
+    await delay(500);
+
+    if (rope.length <= 4) {
+      if ((checkAvail('west') || rope.length === 0) && toWestQueue.length > 0) {
+        toWestQueue[0].crossRavine();
+      }
+      if ((checkAvail('east') || rope.length === 0) && toEastQueue.length > 0) {
+        toEastQueue[0].crossRavine();
+      }
+    }
+
+    console.log(
+      JSON.stringify(
+        rope
+          .map(value => '(' + value.id + ' - ' + value.destination + ')'))
+          .split('","')
+          .join(' ')
+          .replace(/\[\"/, '')
+          .replace(/\"\]/, '')
+    );
   }
 }
 
-exports = module.exports = Pig;
+start();
